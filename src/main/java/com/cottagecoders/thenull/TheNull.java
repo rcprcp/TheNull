bf04949bb6ae9c137db18eeed6bd107179a13d65
bob <bob@streamsets.com>
2020-02-26 17:02:59 -0500

    first commit

---
+++ src/main/java/com/cottagecoders/thenull/TheNull.java
@@ -0,0 +1,21 @@
+package com.cottagecoders.thenull;
+
+public class TheNull {
+
+  public TheNull() {
+
+  }
+
+  public String itsNull() {
+    return null;
+  }
+
+  public String notNull()  {
+    return "hello";
+  }
+
+  public String secetMessage() {
+    return "This is the Secret Message";
+  }
+
+}
